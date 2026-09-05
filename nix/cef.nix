95641767299bd710dc88ba07c5166b5898a2ad38
Romana <science.zip@protonmail.com>
2026-09-05 22:26:52 +0200

    fix: Mirror provinance checks from other platform

--- nix/cef.nix
+++ nix/cef.nix
@@ -1,13 +1,15 @@
 { pkgs, cefVersion }:
 
 let
+  version = cefVersion;
+  gitRevision = "8042e43";
+  chromiumVersion = "150.0.7871.101";
+
   cef = pkgs.cef-binary.override {
-    version = cefVersion;
-    gitRevision = "8042e43";
-    chromiumVersion = "150.0.7871.101";
+    inherit version gitRevision chromiumVersion;
 
     srcHashes = {
-      aarch64-linux = "";
+      aarch64-linux = pkgs.lib.fakeHash;
       x86_64-linux = "sha256-bB1Ike84huPM9l0JKI2DBOP343JKR8kyk+K9Y+dlKOQ=";
     };
   };
@@ -17,8 +19,8 @@ let
     destination = "/archive.json";
     text = builtins.toJSON {
       type = "minimal";
-      name = "cef_binary_${cefVersion}";
-      sha1 = "0000000000000000000000000000000000000000";
+      name = "cef_binary_${cefVersion}+g${gitRevision}+chromium-${chromiumVersion}_NixIsLoveNixIsLife_minimal.tar.bz2";
+      sha1 = pkgs.lib.fakeHash;
     };
   };
 in
